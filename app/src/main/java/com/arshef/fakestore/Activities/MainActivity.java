26a7783319061b43b597a92c9e2a8385ff0f0d63
storm <storm.aref@gmail.com>
2019-05-07 11:27:45 +0430

    sugarorm

--- app/src/main/java/com/arshef/fakestore/Activities/MainActivity.java
+++ app/src/main/java/com/arshef/fakestore/Activities/MainActivity.java
@@ -3,16 +3,33 @@ package com.arshef.fakestore.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.arshef.fakestore.Models.Basket;
 import com.arshef.fakestore.R;
+import com.arshef.fakestore.Tools.DBTools;
+import com.orm.SchemaGenerator;
 import com.orm.SugarContext;
+import com.orm.SugarDb;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    DBTools dbTools;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SugarContext.init(getApplicationContext());
+        dbTools = new DBTools();
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Basket> basketList = dbTools.test();
+            }
+        });
     }
 }
