58a187522c4debdcbc818e3ea67cb12e3bbcd6dc
Arshia Majidi <arshiamajidi@ymail.com>
2019-05-12 10:11:48 +0430

    kir

--- app/src/main/java/com/arshef/fakestore/Activities/RegisterActivity.java
+++ app/src/main/java/com/arshef/fakestore/Activities/RegisterActivity.java
@@ -3,29 +3,27 @@ package com.arshef.fakestore.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.widget.Button;
-import android.widget.EdgeEffect;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.arshef.fakestore.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    Button loginbtn;
+    Button registerbtn;
     EditText username;
     EditText password;
+    EditText confirmpass;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.register_layout);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
-        loginbtn = findViewById(R.id.login_btn);
-
+        confirmpass = findViewById(R.id.confirmpass);
+        registerbtn = findViewById(R.id.register_btn);
 
     }
 }
