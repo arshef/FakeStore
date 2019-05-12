14ecad7b6a8d313834df4d43cea4e29ce8afee59
storm <storm.aref@gmail.com>
2019-05-12 09:16:35 +0430

    Login checker

--- app/src/main/java/com/arshef/fakestore/Activities/LoginActivity.java
+++ app/src/main/java/com/arshef/fakestore/Activities/LoginActivity.java
@@ -3,27 +3,50 @@ package com.arshef.fakestore.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EdgeEffect;
 import android.widget.EditText;
-import android.widget.TextView;
 
+import com.arshef.fakestore.Models.User;
 import com.arshef.fakestore.R;
+import com.arshef.fakestore.Tools.StaticTools;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button login_btn;
-    EditText username;
-    EditText password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.login_layout);
-        username = findViewById("username");
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Authenticate(username.getText().toString())) {
+                    if (Authorize(username.getText().toString(), password.getText().toString())) {
+                    } else {
+                        StaticTools.ToastMaker(LoginActivity.this, "Password is incorrect!");
+                    }
+                } else {
+                    StaticTools.ToastMaker(LoginActivity.this, "User is not available!");
+                }
+            }
+        });
+    }
 
+    private boolean Authenticate(String username) {
+        if (User.find(User.class, "Username = ?", username).size() > 0) {
+            return true;
+        }
+        return false;
+    }
 
+    private boolean Authorize(String username, String password) {
+        if (User.find(User.class, "Username = ? and Password = ?", username, password).size() > 0) {
+            return true;
+        }
+        return false;
     }
 }
