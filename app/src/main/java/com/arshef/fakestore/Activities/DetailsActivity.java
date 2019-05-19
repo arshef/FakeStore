5bf85731cc01004f6f4ce9d95189b2e4014ade4a
Arshia Majidi <arshiamajidi@ymail.com>
2019-05-19 11:39:34 +0430

    kir

--- app/src/main/java/com/arshef/fakestore/Activities/DetailsActivity.java
+++ app/src/main/java/com/arshef/fakestore/Activities/DetailsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.arshef.fakestore.R;
 
@@ -15,6 +16,7 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ImageView prdct_img = findViewById(R.id.product_img);
         Intent intent = getIntent();
         id = intent.getLongExtra("id", -1);
     }
